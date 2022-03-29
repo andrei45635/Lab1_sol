5f9fdbb6df8749d62f5996081190ce415e5c4130
Andrei <andrei.iacob@stud.ubbcluj.ro>
2022-03-30 01:00:34 +0300

    Add project files.

---
+++ DO.cpp
@@ -0,0 +1,114 @@
+#include "Iterator.h"
+#include "DO.h"
+#include <iostream>
+
+#include <exception>
+using namespace std;
+
+DO::DO(Relatie r) {
+	/* de adaugat */
+	elems = new TElem[cap];
+	this->cap = 5;
+	this->lg = 0;
+	this->r = r;
+	//am alocat dinamic vector de elemente
+	// am initializat capacitatea cu 5, iar lungimea cu 0 (nu sunt elemente)
+	//relatia este r
+}
+
+//adauga o pereche (cheie, valoare) in dictionar
+//daca exista deja cheia in dictionar, inlocuieste valoarea asociata cheii si returneaza vechea valoare
+//daca nu exista cheia, adauga perechea si returneaza null
+TValoare DO::adauga(TCheie c, TValoare v) {
+	/* de adaugat */
+
+	//daca lungimea depaseste capacitatea, atunci dublam capacitatea
+	if (lg >= cap) {
+		TElem* new_elems = new TElem[2 * cap];
+		cap = 2 * cap;
+		for (int i = 0; i < lg; i++) {
+			new_elems[i].first = this->elems[i].first;
+			new_elems[i].second = this->elems[i].second;
+		}
+		delete[] elems;
+		elems = new_elems;
+	}
+	//daca exista cheia in dictionar, atunci inlocuim val initiala cu v si returnam val initiala
+	for (int i = 0; i < lg; i++) {
+		if (this->elems[i].first == c) {
+			TValoare old_val = this->elems[i].second;
+			this->elems[i].second = v;
+			return old_val;
+		}
+	}
+	//adaugam perechea in dictionar pe pozitia corecta
+	for (int i = 0; i < lg; i++) {
+		if (r(this->elems[i].first, c) == true) {
+			lg++;
+			for (int j = lg - 1; j > i; j--) {
+				this->elems[j].first = this->elems[j - 1].first;
+				this->elems[j].second = this->elems[j - 1].second;
+			}
+			this->elems[i].first = c;
+			this->elems[i].second = v;
+		}
+		return NULL_TVALOARE;
+	}
+	//adaugam pe ultima pozitie perechea daca nu s-a adaugat
+	lg++;
+	this->elems[lg - 1].first = c;
+	this->elems[lg - 1].second = v;
+	return NULL_TVALOARE;
+}
+
+//cauta o cheie si returneaza valoarea asociata (daca dictionarul contine cheia) sau null
+TValoare DO::cauta(TCheie c) const {
+	/* de adaugat */
+	for (int i = 0; i < lg; i++) {
+		if (this->elems[i].first == c) {
+			return this->elems[i].second;
+		}
+	}
+	return NULL_TVALOARE;
+}
+
+//sterge o cheie si returneaza valoarea asociata (daca exista) sau null
+TValoare DO::sterge(TCheie c) {
+	/* de adaugat */
+	for (int i = 0; i < lg; i++) {
+		if (this->elems[i].first == c) {
+			TValoare val = this->elems[i].second;
+			for (int j = i; j < lg - 1; j++) {
+				this->elems[j].first = this->elems[j+1].first;
+				this->elems[j].second = this->elems[j + 1].second;
+			}
+			lg--;
+			return val;
+		}
+	}
+	return NULL_TVALOARE;
+}
+
+//returneaza numarul de perechi (cheie, valoare) din dictionar
+int DO::dim() const {
+	/* de adaugat */
+	return lg;
+}
+
+//verifica daca dictionarul e vid
+bool DO::vid() const {
+	/* de adaugat */
+	if (lg > 0) {
+		return false;
+	}
+	else return true;
+}
+
+Iterator DO::iterator() const {
+	return  Iterator(*this);
+}
+
+DO::~DO() {
+	/* de adaugat */
+	delete[] elems;
+}
